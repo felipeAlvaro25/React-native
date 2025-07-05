50e54ff427263fe0ba19c18cf0f09c343af3acba
chris12nj <christofer.123002@gmail.com>
2025-07-04 19:32:41 -0500

    Descripción de los cambios realizados

--- app/(proyectos)/index.tsx
+++ app/(proyectos)/index.tsx
@@ -37,6 +37,7 @@ export default function Pantalla1() {
                 <Texto>Hola de nuevo</Texto>
                 <Texto>Hola de nuevo felipe</Texto>
                 <Texto>ya wey soy cesar</Texto>
+                <Texto>07/04</Texto>
                 <StyledInput
                     placeholder="Escribe algo"
                     placeholderTextColor={currentTheme.Colors.text}
