d3704d23ae51e77c2ae090deea59bc5db29e81b4
felipeAlvaro25 <121649661+felipeAlvaro25@users.noreply.github.com>
2025-07-07 23:16:45 -0500

    Correccion de agregar-productos

--- app/(reloj)/_layout.tsx
+++ app/(reloj)/_layout.tsx
@@ -1,10 +1,22 @@
-import { Stack } from "expo-router";
-export default function layout(){
-    return(
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-                <Stack screenOptions={{headerShown:false}}>
-                <Stack.Screen name="index"/>
-                <Stack.Screen name="conversor"/>
-                </Stack>
-    )
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  // Quitamos el useFonts y la carga de la fuente personalizada
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+    </ThemeProvider>
+  );
 }
